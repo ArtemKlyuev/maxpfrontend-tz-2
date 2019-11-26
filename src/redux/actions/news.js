c0a7f27790aec14ab235240e4cdcfcfc9117b261
Artem Klyuev <klyuev@sizmek.ru>
2019-11-26 14:23:51 +0300

    add news fetch

--- src/redux/actions/news.js
+++ src/redux/actions/news.js
@@ -1,46 +1,33 @@
+import axios from '../../shared/axiosInstance';
 import * as actionTypes from './actionTypes';
 
 export const fetchNewsStart = () => ({ type: actionTypes.FETCH_NEWS_START });
 
-export const fetchNewsFail = (error) => ({
-    type: actionTypes.FETCH_NEWS_FAIL,
-    error
-});
+export const fetchNewsFail = () => ({ type: actionTypes.FETCH_NEWS_FAIL });
 
 export const fetchNewsSuccess = (news) => ({
     type: actionTypes.FETCH_NEWS_SUCCESS,
     news
 });
 
-export const fetchNews = () => {
-    return (dispatch) => {
-        dispatch(fetchNewsStart());
+export const fetchNews = () => async (dispatch) => {
+    dispatch(fetchNewsStart());
 
-        const startUrl = 'https://newsapi.org/v2/top-headlines';
-        const country = 'ru';
-        const apikey = '131595506e1e4017aeaddc953c9bcd5d';
-        const url = `${startUrl}?country=${country}&apiKey=${apikey}`;
+    try {
+        const response = await axios.get('/news');
+        const { status, data } = response.data;
 
-        fetch(url)
-            .then((res) => {
-                if (!res.ok) {
-                    console.log('res.statusText', res);
-                    throw new Error(res.statusText);
-                }
-                return res.json();
-            })
-            .then((news) => {
-                console.log('news', news.articles);
-                dispatch(fetchNewsSuccess(news.articles));
-            })
-            .catch((err) => {
-                console.log('fetch news data fail', err);
-                dispatch(fetchNewsFail(err));
-            });
-    };
+        console.log('res news', response);
+
+        if (status !== 'ok') {
+            throw new Error(response.message);
+        }
+
+        console.log('news data', data);
+
+        dispatch(fetchNewsSuccess(data));
+    } catch (error) {
+        console.error('News fetch error', error);
+        dispatch(fetchNewsFail());
+    }
 };
-
-export const moreInfoClicked = (newsItem) => ({
-    type: actionTypes.MORE_INFO_CLICKED,
-    newsItem
-});
