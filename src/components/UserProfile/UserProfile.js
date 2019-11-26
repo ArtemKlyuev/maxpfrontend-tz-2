8ac7c4f238ecb2ce85de51a91b9b16c78b5b5209
Artem Klyuev <klyuev@sizmek.ru>
2019-11-26 12:43:00 +0300

    added user profile page

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -10,13 +10,31 @@ import youtube from '../../assets/youtube.svg';
 const userProfile = (props) => {
     console.log('props user', props);
     const { city, languages, social } = props;
-    // const obj = {
-    //     [social].telegram: telegram,
-    // }
+
+    const imgsObj = {
+        telegram,
+        twitch,
+        twitter,
+        vk,
+        web,
+        youtube
+    };
+
     const langList = languages.map((lang) => <li key={lang}>{lang}</li>);
+
     const socialList = social.map((social) => (
-        <a href={social.link} style={{ backgroundImage: social }}></a>
+        <li key={social.label}>
+            <a
+                href={social.link}
+                className={classes.socialLink}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img src={imgsObj[social.label]} alt={social.label} />
+            </a>
+        </li>
     ));
+
     return (
         <div className={classes.UserProfile}>
             <div>Город: {city}</div>
@@ -24,7 +42,7 @@ const userProfile = (props) => {
                 Знание языков: <ul>{langList}</ul>
             </div>
             <div>
-                Ссылки: <ul></ul>
+                Ссылки: <ul>{socialList}</ul>
             </div>
         </div>
     );
