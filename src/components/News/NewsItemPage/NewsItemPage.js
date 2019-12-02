30df4aaaa5a352aaf597bb2e3fda0126107578fd
Artem <kluv.rus@gmail.com>
2019-12-02 14:05:31 +0300

    remove GoBack button

--- src/components/News/NewsItemPage/NewsItemPage.js
+++ src/components/News/NewsItemPage/NewsItemPage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import NewsItem from '../NewsItem/NewsItem';
-import GoBack from '../../UI/GoBack/GoBack';
 
 const newsItemPage = (props) => {
     if (!props.fullNewsItemToLoad) {
@@ -19,7 +18,6 @@ const newsItemPage = (props) => {
 
     return (
         <NewsItem {...{ publishedAt, title }}>
-            <GoBack history={props.history} />
             <img src={urlToImage} style={{ width: '100%' }} alt="News image" />
             <p>{description}</p>
             <a href={url} target="_blank" rel="noopener noreferrer">
